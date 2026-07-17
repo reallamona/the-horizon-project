822cee9df2510140a20b24c35c0f56d81a62c8d7
Lamona <lamona.content@gmail.com>
2026-07-17 00:36:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const supabaseUrl = "YOUR_SUPABASE_URL";
-const supabaseKey = "YOUR_SUPABASE_KEY";
+const supabaseUrl = "https://keaojtqfsbotsoywydhf.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtlYW9qdHFmc2JvdHNveXd5ZGhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyNTczNzQsImV4cCI6MjA5OTgzMzM3NH0.MR35dddwK-GwvWPG9s2cgTRLKXhAaJYuwHVxlyS3FCo";
 
 const supabase = window.supabase.createClient(
 supabaseUrl,

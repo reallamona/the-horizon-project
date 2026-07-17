7b4a182489297102754b78f7d3c5d440c39a26ff
Lamona <lamona.content@gmail.com>
2026-07-17 00:15:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const supabaseUrl = "YOUR_SUPABASE_URL";
+const supabaseKey = "YOUR_SUPABASE_KEY";
 
+const supabase = window.supabase.createClient(
+supabaseUrl,
+supabaseKey
+);
+
+async function loadGlitches() {
+const { data, error } = await supabase
+.from("glitches")
+.select("*");
+
+if (error) {
+console.log(error);
+return;
+}
+
+console.log(data);
+}
+
+loadGlitches();

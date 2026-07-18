2cb86fb4d9b4469f7bba36845c0707ea216a7799
Lamona <lamona.content@gmail.com>
2026-07-18 09:38:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Load HTML sections
 async function loadSection(id, file) {
     const response = await fetch("sections/" + file);
     const content = await response.text();
@@ -10,3 +11,42 @@ loadSection("community", "community.html");
 loadSection("games", "games.html");
 
 
+// Load JSON resources
+fetch("data/resources.json")
+.then(response => response.json())
+.then(resources => {
+
+    const container = document.getElementById("resources");
+
+    resources.forEach(resource => {
+        container.innerHTML += `
+            <ul>
+                <li>
+                    <a href="${resource.url}" target="_blank">
+                        ${resource.name}
+                    </a>
+                    <p>${resource.category}</p>
+                </li>
+            </ul>
+        `;
+    });
+
+});
+
+
+// Search
+const search = document.getElementById("search");
+
+search.addEventListener("input", function () {
+    const filter = search.value.toLowerCase();
+
+    const items = document.querySelectorAll("li, .game-folder, .resource");
+
+    items.forEach(item => {
+        const text = item.textContent.toLowerCase();
+
+        item.style.display = text.includes(filter)
+            ? ""
+            : "none";
+    });
+});

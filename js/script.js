1f602a1570f108c0c5ae7787653154f84b55e4de
Lamona <lamona.content@gmail.com>
2026-07-18 09:54:35 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+async function loadSection(id, file) {
+    const response = await fetch("sections/" + file);
+    const content = await response.text();
 
+    document.getElementById(id).innerHTML = content;
+}
+
+loadSection("leaderboards", "leaderboards.html");
+loadSection("community", "community.html");
+loadSection("games", "games.html");
+
+
+const search = document.getElementById("search");
+
+search.addEventListener("input", function () {
+    const filter = search.value.toLowerCase();
+
+    const items = document.querySelectorAll("li, .game-folder");
+
+    items.forEach(item => {
+        item.style.display = item.textContent.toLowerCase().includes(filter)
+            ? ""
+            : "none";
+    });
+});

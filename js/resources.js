065be478d4471a867080434f7917e05599be50f8
Lamona <lamona.content@gmail.com>
2026-07-18 09:31:33 -0500

    Update resources.js

--- js/resources.js
+++ js/resources.js
@@ -1,1 +1,18 @@
+fetch("data/resources.json")
+.then(response => response.json())
+.then(resources => {
 
+    const container = document.getElementById("resources");
+
+    resources.forEach(resource => {
+        container.innerHTML += `
+            <div class="resource">
+                <a href="${resource.url}" target="_blank">
+                    ${resource.name}
+                </a>
+                <p>${resource.category}</p>
+            </div>
+        `;
+    });
+
+});

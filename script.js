ba43edd131d563a8b473a91577e89efd5dac8ea5
Lamona <lamona.content@gmail.com>
2026-07-18 08:55:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+async function loadSection(id, file) {
+    const response = await fetch("sections/" + file);
+    const content = await response.text();
+
+    document.getElementById(id).innerHTML = content;
+}
+
+loadSection("leaderboards", "leaderboards.html");
+loadSection("community", "community.html");
+loadSection("games", "games.html");
+
+
 const search = document.getElementById("search");
 
 search.addEventListener("input", function () {

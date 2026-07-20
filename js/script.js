1b03d28106302b98245bf4de839b2dc67f883a69
Lamona <lamona.content@gmail.com>
2026-07-19 19:16:38 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,24 +14,6 @@ async function loadSection(id, file) {
     }
 }
 
-function setupToggleButton() {
-    const gamesSection = document.getElementById("games");
-    const button = gamesSection.querySelector("#toggleAll");
-
-    if (!button) return;
-
-    button.addEventListener("click", () => {
-        const folders = gamesSection.querySelectorAll(".game-folder");
-        const expand = button.textContent === "Expand All";
-
-        folders.forEach(folder => {
-            folder.open = expand;
-        });
-
-        button.textContent = expand ? "Collapse All" : "Expand All";
-    });
-}
-
 /* Init Loading */
 async function init() {
     await Promise.all([
